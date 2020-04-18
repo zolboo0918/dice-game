4ec878e51fb449ce218398a4413f71d79983c1d3
zolboo <zolboo412@gmail.com>
2020-04-18 18:35:13 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ var scores = [0, 0];
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах оноо
 var roundScore = 0;
 //Шооны аль талаараа буусныг хадгалах хувьсагч, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүй үүсгэж өгнө.
-var dice = Math.ceil(Math.random() * 6);
 
 //Програм эхлэхэд бэлтгэе
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = "0";
 
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").innerHTML = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").innerHTML = "0";
+document.getElementById("current-0").innerHTML = "0";
+document.getElementById("current-1").innerHTML = "0";
 
-document.querySelector("#current-0").innerHTML = 0;
-document.querySelector("#current-1").innerHTML = 0;
+var dice = document.querySelector(".dice");
+dice.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.ceil(Math.random() * 6);
+  dice.style.display = "block";
+  dice.src = "dice-" + diceNumber + ".png";
+});
