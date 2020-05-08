2e289e9c58a1e95277bfb5c1b21cd6863abce918
zolboo0918 <zolboo412@gmail.com>
2020-05-08 17:24:33 +0800

    Mongol heltei bolson

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ function initGame() {
   document.querySelector(".player-0-panel").classList.remove(".active");
   document.querySelector(".player-1-panel").classList.remove(".active");
 
-  document.getElementById("name-0").textContent = "Player 1";
-  document.getElementById("name-1").textContent = "Player 2";
+  document.getElementById("name-0").textContent = "Тоглогч 1";
+  document.getElementById("name-1").textContent = "Тоглогч 2";
 
   document.querySelector(".player-0-panel").classList.add(".active");
 
