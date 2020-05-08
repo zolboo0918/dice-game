a887e78ac409d022baff61c207d996b16266f78f
zolboo <zolboo412@gmail.com>
2020-05-08 16:57:08 +0800

    End of all

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //Шооны зургийг үзүүлэх элементийг DOM-оос хайж олоод энд хадгална.
 var dice = document.querySelector(".dice");
+
 var activePlayer;
 var scores;
 var roundScore;
+var isNewGame;
 function initGame() {
   // Тоглогчийн ээлжийг хадгалах хувьсагч, 1-р тоглогчийн 0, 2-р тоглогчийг 1 гэж тэмдэглэе
   activePlayer = 0;
@@ -18,44 +20,60 @@ function initGame() {
   document.getElementById("current-0").textContent = "0";
   document.getElementById("current-1").textContent = "0";
 
+  document.querySelector(".player-0-panel").classList.remove(".winner");
+  document.querySelector(".player-1-panel").classList.remove(".winner");
+
+  document.querySelector(".player-0-panel").classList.remove(".active");
+  document.querySelector(".player-1-panel").classList.remove(".active");
+
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
+
+  document.querySelector(".player-0-panel").classList.add(".active");
+
+  isNewGame = true;
+
   dice.style.display = "none";
-
-  document.querySelector(".player-0-panel").classList.remove("winner");
-  document.querySelector(".player-1-panel").classList.remove("winner");
-
-  document.querySelector(".player-0-panel").classList.remove("active");
-  document.querySelector(".player-1-panel").classList.remove("active");
-
-  document.querySelector(".player-0-panel").classList.add("active");
 }
 
 initGame();
 
 //Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function () {
-  var diceNumber = Math.ceil(Math.random() * 6);
-  dice.style.display = "block";
-  dice.src = "dice-" + diceNumber + ".png";
-  //Буусан тоо нь 1-ээс ялгаатай бол Тоглогчийн ээлжийн оноог өөрчилнө.
-  if (diceNumber !== 1) {
-    roundScore += diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  if (isNewGame) {
+    var diceNumber = Math.ceil(Math.random() * 6);
+    dice.style.display = "block";
+    dice.src = "dice-" + diceNumber + ".png";
+    //Буусан тоо нь 1-ээс ялгаатай бол Тоглогчийн ээлжийн оноог өөрчилнө.
+    if (diceNumber !== 1) {
+      roundScore += diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      changePlayer();
+    }
   } else {
-    changePlayer();
+    alert("Тоглоом дууссан байна. Тоглоомыг шинээр эхлүүлнэ үү.");
   }
 });
 //Hold tochnii eventListener
 document.querySelector(".btn-hold").addEventListener("click", function () {
-  scores[activePlayer] += roundScore;
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
-  if (scores[activePlayer] >= 10) {
-    document.getElementById("name-" + activePlayer).textContent = "WINNER!!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
+  if (isNewGame) {
+    scores[activePlayer] += roundScore;
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 50) {
+      isNewGame = false;
+      document.getElementById("name-" + activePlayer).textContent = "WINNER!!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+    } else {
+      changePlayer();
+    }
   } else {
-    changePlayer();
+    alert("Тоглоом дууссан байна. Тоглоомыг шинээр эхлүүлнэ үү.");
   }
 });
 
